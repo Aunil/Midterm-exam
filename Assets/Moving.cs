e5c4aa46b0f064853d83b4f6d6a54e50c99bc201
Ravi Patel <ravipatelnov1@gmail.com>
2018-12-21 11:53:32 -0500

    Initial commit

---
+++ Assets/Moving.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moving : MonoBehaviour {
+	public float velocity = 20;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.velocity = new Vector3 (0.0f, 0.0f, velocity);
+
+	}
+	void OnTriggerEnter(){
+		//rb.velocity = new Vector3 (0.0f, 0.0f, -1);
+		velocity = velocity* -1;
+		rb.velocity = new Vector3 (0.0f, 0.0f, velocity);
+	}
+}
